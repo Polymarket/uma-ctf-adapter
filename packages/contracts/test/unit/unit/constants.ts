21a73591f0e25411f772dc60618b5c0bbbc332fd
JonathanAmenechi <7217608+JonathanAmenechi@users.noreply.github.com>
2021-08-30 14:02:35 -0400

    chore: remove unused test

--- packages/contracts/test/unit/unit/constants.ts
+++
@@ -1,4 +0,0 @@
-export const QUESTION_TITLE = "This is a mock question title?";
-export const DESC =
-    "This is a binary market on the mock question title. p1: 0, p2: 1. Where p2 corresponds to YES, p1 to a NO";
-export const thirtyDays = 30 * 24 * 60 * 60;
