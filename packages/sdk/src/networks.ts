d36f8fa5f5148c0bfd10599cabc42b482b6e54d6
JonathanAmenechi <7217608+JonathanAmenechi@users.noreply.github.com>
2021-10-08 17:31:57 -0400

    fix: update adapter address

--- packages/sdk/src/networks.ts
+++ packages/sdk/src/networks.ts
@@ -3,7 +3,7 @@ export const getAdapterAddress = (chainID: number): string => {
         case 137:
             return "0xf1a3470Ea4943783d680ec2A8a05aE517684513D";
         case 80001:
-            return "0x46546DF11921a781c2e44C370163263E15b739A9";
+            return "0xB312926d202e93A1Bd35a393E5922Cec843C2792";
         default:
             throw new Error(`Unsupported chainID: ${chainID}`);
     }
