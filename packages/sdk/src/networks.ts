2eafa5b22c57b53d8f24e7e94e835c59ec087c24
JonathanAmenechi <7217608+JonathanAmenechi@users.noreply.github.com>
2021-10-19 12:19:36 -0400

    feat: matic deployment

--- packages/sdk/src/networks.ts
+++ packages/sdk/src/networks.ts
@@ -1,7 +1,7 @@
 export const getAdapterAddress = (chainID: number): string => {
     switch (chainID) {
         case 137:
-            return "0xf1a3470Ea4943783d680ec2A8a05aE517684513D";
+            return "0x132F6dd49DF50626e27685985B1c1B133e331b55";
         case 80001:
             return "0x4F8070c5a6dF56286F9b9A5B1A3AfeE437736567";
         default:
