b44237995e5cb270edb8dc3fffc5ec7eadfa0cfb
JonathanAmenechi <7217608+JonathanAmenechi@users.noreply.github.com>
2021-12-13 12:02:30 -0500

    chore: spacing

--- packages/contracts/contracts/UmaConditionalTokensBinaryAdapter.sol
+++ packages/contracts/contracts/UmaConditionalTokensBinaryAdapter.sol
@@ -3,7 +3,6 @@ pragma solidity 0.8.0;
 
 import { AccessControl } from "@openzeppelin/contracts/access/AccessControl.sol";
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-
 import { IConditionalTokens } from "./interfaces/IConditionalTokens.sol";
 import { FinderInterface } from "./interfaces/FinderInterface.sol";
 import { OptimisticOracleInterface } from "./interfaces/OptimisticOracleInterface.sol";
