0cae3a50ed7711fcf45ec76fbd01699f41c0b71e
JonathanAmenechi <7217608+JonathanAmenechi@users.noreply.github.com>
2022-01-08 15:35:00 -0500

    fix: pass oo to _resetQuestion

--- packages/contracts/contracts/UmaConditionalTokensBinaryAdapter.sol
+++ packages/contracts/contracts/UmaConditionalTokensBinaryAdapter.sol
@@ -383,7 +383,7 @@ contract UmaConditionalTokensBinaryAdapter is ReentrancyGuard {
 
         // NOTE: If the proposed price is the ignore price, reset the question, allowing new resolution requests
         if (proposedPrice == ignorePrice()) {
-            _resetQuestion(questionID, questionData);
+            _resetQuestion(questionID, questionData, optimisticOracle);
             return;
         }
 
@@ -409,7 +409,7 @@ contract UmaConditionalTokensBinaryAdapter is ReentrancyGuard {
 
         // NOTE: If the proposed price is the ignore price, reset the question, allowing new resolution requests
         if (proposedPrice == ignorePrice()) {
-            _resetQuestion(questionID, questionData);
+            _resetQuestion(questionID, questionData, optimisticOracle);
             return;
         }
 
@@ -425,8 +425,11 @@ contract UmaConditionalTokensBinaryAdapter is ReentrancyGuard {
         emit QuestionSettled(questionID, settledPrice, true);
     }
 
-    function _resetQuestion(bytes32 questionID, QuestionData storage questionData) internal {
-        OptimisticOracleInterface optimisticOracle = getOptimisticOracle();
+    function _resetQuestion(
+        bytes32 questionID,
+        QuestionData storage questionData,
+        OptimisticOracleInterface optimisticOracle
+    ) internal {
         optimisticOracle.settleAndGetPrice(identifier, _getTimestamp(questionData), questionData.ancillaryData);
 
         questionData.earlyResolutionTimestamp = 0;
