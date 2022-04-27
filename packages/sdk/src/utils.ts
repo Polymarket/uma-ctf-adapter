3fbb44154b947377729e0f53a4bc3c7a898b418d
Niraek Jain-Sharma <niraekjs@berkeley.edu>
2022-04-27 14:24:05 -0400

    redone p4 ancillary

--- packages/sdk/src/utils.ts
+++ packages/sdk/src/utils.ts
@@ -9,10 +9,10 @@ import { ethers } from "ethers";
  */
 export const buildResolutionData = (outcomes: string[]): string => {
     return `p1: 0, p2: 1, p3: 0.5, p4: -57896044618658097711785492504343953926634992332820282019728.792003956564819968.
-    Where p1 corresponds to ${outcomes[1]}, p2 to ${outcomes[0]}, p3 to unknown, and p4 to an early request`;
+    Where p1 corresponds to ${outcomes[1]}, p2 to a ${outcomes[0]}, p3 to unknown, and p4 to an early request`;
 }
 
-export const OUTCOME_REGEX = /Where p2 corresponds to (\w+), p1 to a (\w+)/;
+export const OUTCOME_REGEX = /Where p1 corresponds to (\w+), p2 to a (\w+)/;
 
 export const extractOutcomes = (ancillaryDataString: string): string[] | null => {
     const matched = ancillaryDataString.match(OUTCOME_REGEX);
