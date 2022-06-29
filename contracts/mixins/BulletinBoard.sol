cb5b00ab34f9eca8faa91254a3b1bbc88fc2821e
JonathanAmenechi <7217608+JonathanAmenechi@users.noreply.github.com>
2022-06-28 21:20:03 -0400

    fix: todos, getLatestUpdate does not revert in err case

--- contracts/mixins/BulletinBoard.sol
+++ contracts/mixins/BulletinBoard.sol
@@ -2,30 +2,45 @@
 pragma solidity ^0.8.10;
 
 /// @title Bulletin Board
-/// @notice An onchain registry containing updates for questions
+/// @notice A registry containing ancillary data updates for questions
 abstract contract BulletinBoard {
-    struct Update {
+    struct AncillaryDataUpdate {
         uint256 timestamp;
         bytes update;
     }
 
-    /// @notice TODO natspec
-    mapping(bytes32 => Update[]) public updates;
+    /// @notice Mapping to an array of Ancillary data updates for questions
+    mapping(bytes32 => AncillaryDataUpdate[]) public updates;
 
-    /// @notice TODO natspec
+    /// @notice Emitted when an ancillary data update is posted
+    event AncillaryDataUpdated(bytes32 indexed questionID, address indexed owner, bytes update);
+
+    /// @notice Post an update for the question
+    /// @param questionID   - The unique questionID
+    /// @param update       - The update for the question
     function postUpdate(bytes32 questionID, bytes memory update) external {
         bytes32 id = keccak256(abi.encode(questionID, msg.sender));
-        updates[id].push(Update({ timestamp: block.timestamp, update: update }));
+        updates[id].push(AncillaryDataUpdate({ timestamp: block.timestamp, update: update }));
     }
 
-    /// @notice TODO natspec
-    function getUpdates(bytes32 questionID, address owner) public view returns (Update[] memory) {
+    /// @notice Gets all updates for a questionID and owner
+    /// @param questionID   - The unique questionID
+    /// @param owner        - The address of the question initializer
+    function getUpdates(bytes32 questionID, address owner) public view returns (AncillaryDataUpdate[] memory) {
         return updates[keccak256(abi.encode(questionID, owner))];
     }
 
-    /// @notice TODO natspec
-    function getLatestUpdate(bytes32 questionID, address owner) external view returns (Update memory) {
-        Update[] memory currentUpdates = getUpdates(questionID, owner);
+    /// @notice Gets the latest update for a questionID and owner
+    /// @param questionID   - The unique questionID
+    /// @param owner        - The address of the question initializer
+    function getLatestUpdate(bytes32 questionID, address owner) external view returns (AncillaryDataUpdate memory) {
+        AncillaryDataUpdate[] memory currentUpdates = getUpdates(questionID, owner);
+        if (currentUpdates.length == 0) {
+            return AncillaryDataUpdate({
+                timestamp: 0,
+                update: ""
+            });
+        }
         return currentUpdates[currentUpdates.length - 1];
     }
 }
