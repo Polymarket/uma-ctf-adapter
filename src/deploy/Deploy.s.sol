ea9a59065bcb03e0badc53f5d3305fa9a88e7765
JonathanAmenechi <7217608+JonathanAmenechi@users.noreply.github.com>
2022-10-13 17:42:49 -0400

    fix: deployment scripts

---
+++ src/deploy/Deploy.s.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.15;
+
+import { Script } from "forge-std/Script.sol";
+import { UmaCtfAdapter } from "src/UmaCtfAdapter.sol";
+
+/// @title Deploy
+/// @notice Script to deploy the UmaCtfAdapter
+/// @author Polymarket
+contract Deploy is Script {
+    /// @notice Deploys the Adapter
+    /// @param ctf          - The ConditionalTokens Framework address
+    /// @param finder       - The UMA Finder address
+    function deploy(
+        address ctf,
+        address finder
+    ) public returns (address adapter) {
+        vm.startBroadcast();
+        adapter = address(new UmaCtfAdapter(ctf, finder));
+    }
+}
