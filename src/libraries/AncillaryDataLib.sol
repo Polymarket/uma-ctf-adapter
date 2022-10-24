24d8a4c5ad1edc0eb34d06ff3451f3c7a0167df0
JonathanAmenechi <7217608+JonathanAmenechi@users.noreply.github.com>
2022-10-24 14:07:11 -0400

    chore: visibility on library constant

--- src/libraries/AncillaryDataLib.sol
+++ src/libraries/AncillaryDataLib.sol
@@ -3,7 +3,7 @@ pragma solidity 0.8.15;
 
 library AncillaryDataLib {
 
-    string public constant initializerPrefix = ",initializer:";
+    string private constant initializerPrefix = ",initializer:";
 
     /// @notice Appends the initializer address to the ancillaryData
     /// @param initializer      - The initializer address
